17596253899053ed8f925754154b296c7b50d578
Eytyy <e.tayyem@gmail.com>
2019-11-12 23:02:18 +0200

    refactored VisualizationControls class component to a function component

--- src/js/Controller/Visualization/VisualizationControls.js
+++ src/js/Controller/Visualization/VisualizationControls.js
@@ -1,25 +1,30 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import population from "../../../data/population.1";
 import { format } from "d3";
 
 import { broadcastEvent } from "../../api";
 
-class VisualizationControls extends Component {
-  state = {
+const VisualizationControls = () => {
+  const [state, setState] = useState({
     activeYear: "1960",
     intro: true
-  };
-  years = ["1960", "1970", "1980", "1990", "2000", "2010", "2019"];
+  });
 
-  onYearClick = e => {
+  const updateState = changes => {
+    setState(prevState => ({
+      ...prevState,
+      ...changes
+    }));
+  };
+
+  const years = ["1960", "1970", "1980", "1990", "2000", "2010", "2019"];
+
+  const onYearClick = e => {
     const nextYear = e.currentTarget.id;
 
-    if (this.state.intro) {
-      this.toggleIntro();
-    }
+    if (state.intro) toggleIntro();
 
-    this.setState({
+    updateState({
       activeYear: nextYear
     });
 
@@ -30,9 +35,9 @@ class VisualizationControls extends Component {
     });
   };
 
-  toggleIntro = () => {
-    const intro = !this.state.intro;
-    this.setState({
+  const toggleIntro = () => {
+    const intro = !state.intro;
+    updateState({
       intro
     });
     broadcastEvent({
@@ -42,75 +47,70 @@ class VisualizationControls extends Component {
     });
   };
 
-  onIntroClick = () => {
-    if (this.state.intro) {
-      return;
-    }
-    this.toggleIntro();
+  const onIntroClick = () => {
+    if (state.intro) return;
+    toggleIntro();
   };
 
-  getPopulation = () => {
+  const getPopulation = () => {
     return format(",.0f")(
-      population.find(
-        ({ year }) => year === parseInt(this.state.activeYear, 10)
-      ).population
+      population.find(({ year }) => year === parseInt(state.activeYear, 10))
+        .population
     ).replace(/,/g, " ");
   };
 
-  render() {
-    return (
-      <div className="controller controller__data-viz">
-        <section className="controller__section controller__section--intro">
-          <button
-            className={`intro__btn ${
-              this.state.intro
-                ? "controller__btn controller__btn--active"
-                : "controller__btn"
-            }`}
-            onClick={this.onIntroClick}
-          >
-            {"Intro"}
-          </button>
-        </section>
-        <section className="controller__section controller__section--timeline">
-          <h2 className="controller__sub-title">Story &amp; Projections</h2>
-          <div className="timeline__controls">
-            {this.years.map(year => (
-              <button
-                id={year}
-                key={year}
-                className={`timeline__time ${
-                  !this.state.intro && this.state.activeYear === year
-                    ? "controller__btn controller__btn--active"
-                    : "controller__btn"
-                }`}
-                onClick={this.onYearClick}
-              >
-                {year}
-              </button>
-            ))}
+  return (
+    <div className="controller controller__data-viz">
+      <section className="controller__section controller__section--intro">
+        <button
+          className={`intro__btn ${
+            state.intro
+              ? "controller__btn controller__btn--active"
+              : "controller__btn"
+          }`}
+          onClick={onIntroClick}
+        >
+          {"Intro"}
+        </button>
+      </section>
+      <section className="controller__section controller__section--timeline">
+        <h2 className="controller__sub-title">Story &amp; Projections</h2>
+        <div className="timeline__controls">
+          {years.map(year => (
+            <button
+              id={year}
+              key={year}
+              className={`timeline__time ${
+                !state.intro && state.activeYear === year
+                  ? "controller__btn controller__btn--active"
+                  : "controller__btn"
+              }`}
+              onClick={onYearClick}
+            >
+              {year}
+            </button>
+          ))}
+        </div>
+      </section>
+      <section className="controller__section controller__section--data">
+        <div className="controller__section--data__item">
+          <h2 className="controller__sub-title">Water Levels</h2>
+          <div className="data__item__content">
+            <span className="data__item__content__value">--</span>
+            <span className="data__item__content__label"> mcm</span>
           </div>
-        </section>
-        <section className="controller__section controller__section--data">
-          <div className="controller__section--data__item">
-            <h2 className="controller__sub-title">Water Levels</h2>
-            <div className="data__item__content">
-              <span className="data__item__content__value">--</span>
-              <span className="data__item__content__label"> mcm</span>
-            </div>
+        </div>
+        <div className="controller__section--data__item data__population">
+          <h2 className="controller__sub-title">Population</h2>
+          <div className="data__item__content">
+            <span className="data__item__content__value">
+              {state.intro ? "--" : getPopulation()}
+            </span>
           </div>
-          <div className="controller__section--data__item data__population">
-            <h2 className="controller__sub-title">Population</h2>
-            <div className="data__item__content">
-              <span className="data__item__content__value">
-                {this.state.intro ? "--" : this.getPopulation()}
-              </span>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
-}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default VisualizationControls;
